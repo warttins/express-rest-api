8d78cdfc6a5eec0b63378f64e0f929f8ca81ad43
Diogo Martins <emidemartins@proton.me>
2023-10-21 15:09:46 -0300

    Add products route on server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const { productsRoute } = require('./routes');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.get('/', (_request, response) => {
   response.send('Hello world!');
 });
 
+app.use('/products', productsRoute);
+
 const PORT = 3000;
 
+// eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.\nAccess: http://localhost:${PORT}`));
