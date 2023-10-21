14c0bdc2ab62440e6117c11373730faa55a30353
Diogo Martins <emidemartins@proton.me>
2023-10-21 10:34:28 -0300

    Create products and categories mocks

---
+++ src/mocks/categories.js
@@ -0,0 +1,13 @@
+const categories = [
+  {
+    id: '10000000-0000-0000-0000-000000000cat',
+    name: 'pens',
+  },
+
+  {
+    id: '20000000-0000-0000-0000-000000000cat',
+    name: 'pencils',
+  },
+];
+
+module.exports = categories;
---
+++ src/mocks/products.js
@@ -0,0 +1,24 @@
+const products = [
+  {
+    id: '10000000-0000-0000-0000-00000000prod',
+    color: 'black',
+    price: 2,
+    category_id: '10000000-0000-0000-0000-000000000cat',
+  },
+
+  {
+    id: '20000000-0000-0000-0000-00000000prod',
+    color: 'blue',
+    price: 2,
+    category_id: '10000000-0000-0000-0000-000000000cat',
+  },
+
+  {
+    id: '30000000-0000-0000-0000-00000000prod',
+    color: 'red',
+    price: 1,
+    category_id: '20000000-0000-0000-0000-000000000cat',
+  },
+];
+
+module.exports = products;
